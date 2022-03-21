6d7fb44137652f9226fdb7aa9e4552f163091d62
Adecoded <adebisidrey@gmail.com>
2022-03-21 14:24:33 +0100

    details under progress

--- src/Components/Viewers.js
+++ src/Components/Viewers.js
@@ -38,6 +38,7 @@ grid-template-columns:repeat(5, minmax(0,1fr));
 `
 const Wrap =styled.div`
 border-radius:10px;
+cursor:pointer;
 border:3px solid rgba(249, 249, 249, 0.1);
 box-shadow:rgb(0 0 0 / 69%) 0px 26px 30px -10px,
 rgb(0 0 0 / 73%) 0px 16px 10px -10px;
